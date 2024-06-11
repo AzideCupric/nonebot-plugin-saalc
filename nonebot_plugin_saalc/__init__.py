c2102f6c7e1d96d950fde7e3fd31cbd42dde0cb9
Azide <rukuy@qq.com>
2024-06-11 23:59:43 +0800

    :bug: 正确设置项目名

--- nonebot_plugin_saalc/__init__.py
+++ nonebot_plugin_saalc/__init__.py
@@ -5,7 +5,7 @@ from .message import UniMessageFactory as UniMessageFactory
 
 # 插件元数据，填写规范：https://nonebot.dev/docs/next/advanced/plugin-info#%E6%8F%92%E4%BB%B6%E5%85%83%E6%95%B0%E6%8D%AE
 __plugin_meta__ = PluginMetadata(
-    name="AzideCupric",
+    name="峯驰外包",
     description="使用 SAA 风格混合发送 SAA 和 alc 的消息",
     usage="参见仓库 README",
     homepage="https://github.com/AzideCupric/nonebot-plugin-saalc",
